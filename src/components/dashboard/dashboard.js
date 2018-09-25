857f911650d853ff9987aaa198a266d71231af72
Renee Sarley <reneesarley@gmail.com>
2018-09-25 09:09:17 -0700

    add dashboard components

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component{
+  render(){
+    return(
+      <div className='container'>
+        this is the dashboard
+
+      </div>
+    );
+  }
+
+}
