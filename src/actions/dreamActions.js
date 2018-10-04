8614fb5cecc10f38badef3f9635d6bc5a449ea81
Renee Sarley <reneesarley@gmail.com>
2018-10-04 15:12:53 -0700

    update dreamAction to remove syntax error

--- src/actions/dreamActions.js
+++ src/actions/dreamActions.js
@@ -4,19 +4,20 @@ export const addDream = (dream)=> {
   return (dispatch, getState, {getFirebase, getFirestore}) => {
     //async call
     const firestore = getFirestore();
+
     firestore.collection('dreams').add({
       ...dream,
       status: "still dreaming",
-    }).then(() => {
-      dispatch({
-        type: 'ADD_DREAM',
-        dreamName: dream.dreamName,
-        dreamDetails: dream.dreamDetails,
-        dreamImage: dream.dreamImage,
-        id: v4(),
-      }).catch(err => {
-        dispatch({type: 'ADD_DREAM_ERROR'},err)
-      })
+      }).then(() => {
+          dispatch({
+            type: 'ADD_DREAM',
+            dreamName: dream.dreamName,
+            dreamDetails: dream.dreamDetails,
+            dreamImage: dream.dreamImage,
+            id: v4()
+          });
+        }).catch(err => {
+          dispatch({type: 'ADD_DREAM_ERROR'}, err);
     });
   }
 };
